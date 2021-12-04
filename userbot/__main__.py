955c39c0655d8cd803ed14be8537b32798836b48
Waruserbot <95116492+Waruserbot@users.noreply.github.com>
2021-12-04 21:10:09 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -50,8 +50,8 @@ async def startup_process():
     print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
     print("Yay your userbot is officially working.!!!")
     print(
-        f"Congratulation, now type {cmdhr}alive to see message if catub is live\
-        \nIf you need assistance, head to https://t.me/catuserbot_support"
+        f"Congratulation, now type {cmdhr}alive to see message if warub is live\
+        \nIf you need assistance, head to https://t.me/warbotzspport"
     )
     print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
     await verifyLoggerGroup()
