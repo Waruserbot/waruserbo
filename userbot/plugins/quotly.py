3b7e2b46edd01d746d304723f91dbb0069460c6f
Abhishek singh <84198635+xAbhish3k@users.noreply.github.com>
2021-12-06 22:36:10 +0530

    Update quotly.py

--- userbot/plugins/quotly.py
+++ userbot/plugins/quotly.py
@@ -41,7 +41,7 @@ def get_warp_length(width):
             "-s": "To output file as sticker",
         },
         "usage": "{tr}qpic <flag> <input/reply to text msg>",
-        "examples": ["{tr}qpic CatUserbot.", "{tr}qpic -b CatUserbot."],
+        "examples": ["{tr}qpic WarUserbot.", "{tr}qpic -b WarUserbot."],
     },
 )
 async def q_pic(event):  # sourcery no-metrics
@@ -126,10 +126,10 @@ async def q_pic(event):  # sourcery no-metrics
         )
     output = io.BytesIO()
     if sticker:
-        output.name = "CatUserbot.Webp"
+        output.name = "WarUserbot.Webp"
         img.save(output, "webp")
     else:
-        output.name = "CatUserbot.png"
+        output.name = "WarUserbot.png"
         img.save(output, "PNG")
     output.seek(0)
     await event.client.send_file(event.chat_id, output, reply_to=reply_to)
