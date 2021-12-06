60aaab7835480b471fb8b8bfccd7d364db155c96
Abhishek singh <84198635+xAbhish3k@users.noreply.github.com>
2021-12-06 22:33:44 +0530

    Update sudo.py

--- userbot/plugins/sudo.py
+++ userbot/plugins/sudo.py
@@ -38,13 +38,13 @@ def get_key(val):
     pattern="sudo (on|off)$",
     command=("sudo", plugin_category),
     info={
-        "header": "To enable or disable sudo of your Catuserbot.",
+        "header": "To enable or disable sudo of your Waruserbot.",
         "description": "Initially all sudo commands are disabled, you need to enable them by addscmd\n Check `{tr}help -c addscmd`",
         "usage": "{tr}sudo <on/off>",
     },
 )
 async def chat_blacklist(event):
-    "To enable or disable sudo of your CatUserbot."
+    "To enable or disable sudo of your WarUserbot."
     input_str = event.pattern_match.group(1)
     sudousers = _sudousers_list()
     if input_str == "on":
@@ -174,9 +174,9 @@ async def _(event):
         sudousers = {}
     if len(sudochats) == 0:
         return await edit_delete(
-            event, "__There are no sudo users for your Catuserbot.__"
+            event, "__There are no sudo users for your Waruserbot.__"
         )
-    result = "**The list of sudo users for your Catuserbot are :**\n\n"
+    result = "**The list of sudo users for your WarUserbot are :**\n\n"
     for chat in sudochats:
         result += f"☞ **Name:** {mentionuser(sudousers[str(chat)]['chat_name'],sudousers[str(chat)]['chat_id'])}\n"
         result += f"**Chat Id :** `{chat}`\n"
@@ -255,7 +255,7 @@ async def _(event):  # sourcery no-metrics
         for plugin in input_str:
             if plugin not in PLG_INFO:
                 errors += (
-                    f"`{plugin}` __There is no such plugin in your CatUserbot__.\n"
+                    f"`{plugin}` __There is no such plugin in your WarUserbot__.\n"
                 )
             else:
                 loadcmds += PLG_INFO[plugin]
@@ -264,7 +264,7 @@ async def _(event):  # sourcery no-metrics
         loadcmds = []
         for cmd in input_str:
             if cmd not in CMD_INFO:
-                errors += f"`{cmd}` __There is no such command in your CatUserbot__.\n"
+                errors += f"`{cmd}` __There is no such command in your WarUserbot__.\n"
             elif cmd in sudocmds:
                 errors += f"`{cmd}` __Is already enabled for sudo users__.\n"
             else:
@@ -346,7 +346,7 @@ async def _(event):  # sourcery no-metrics
         for plugin in input_str:
             if plugin not in PLG_INFO:
                 errors += (
-                    f"`{plugin}` __There is no such plugin in your CatUserbot__.\n"
+                    f"`{plugin}` __There is no such plugin in your WarUserbot__.\n"
                 )
             else:
                 flagcmds += PLG_INFO[plugin]
@@ -355,7 +355,7 @@ async def _(event):  # sourcery no-metrics
         flagcmds = []
         for cmd in input_str:
             if cmd not in CMD_INFO:
-                errors += f"`{cmd}` __There is no such command in your CatUserbot__.\n"
+                errors += f"`{cmd}` __There is no such command in your WarUserbot__.\n"
             elif cmd not in sudocmds:
                 errors += f"`{cmd}` __Is already disabled for sudo users__.\n"
             else:
@@ -365,7 +365,7 @@ async def _(event):  # sourcery no-metrics
         if sqllist.is_in_list("sudo_enabled_cmds", cmd):
             count += 1
             sqllist.rm_from_list("sudo_enabled_cmds", cmd)
-    result = f"__Successfully disabled __ `{count}` __ for CatUserbot sudo.__\n"
+    result = f"__Successfully disabled __ `{count}` __ for WarUserbot sudo.__\n"
     output = (
         result + "**Bot is reloading to apply the changes. Please wait for a minute**\n"
     )
