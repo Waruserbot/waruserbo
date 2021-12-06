b0fa2d46baa68856987f09efc274a9d4e22e417d
Abhishek singh <84198635+xAbhish3k@users.noreply.github.com>
2021-12-07 01:03:20 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -61,7 +61,7 @@ class Config(object):
     # set this will channel id of your custom plugins
     PLUGIN_CHANNEL = int(os.environ.get("PLUGIN_CHANNEL") or 0)
     # set this value with your required name for telegraph plugin
-    TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", "catuserbot")
+    TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", "waruserbot")
     # for custom thumb image set this with your required thumb telegraoh link
     THUMB_IMAGE = os.environ.get(
         "THUMB_IMAGE", "https://telegra.ph/file/0bfcbfcb2ae247117a77b.jpg"
