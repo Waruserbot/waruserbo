f469944778f65241fd49a9e62570a1aadc1c8ce9
Abhishek singh <84198635+xAbhish3k@users.noreply.github.com>
2021-12-06 18:23:35 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -9,21 +9,21 @@ from .sql_helper.globals import addgvar, delgvar, gvarstatus
 
 __version__ = "3.0.5"
 __license__ = "GNU Affero General Public License v3.0"
-__author__ = "CatUserBot <https://github.com/waruserbot/catuserbot>"
+__author__ = "WarUserBot <https://github.com/xAbhish3k/waruserbot>"
 __copyright__ = "CatUserBot Copyright (C) 2020 - 2021  " + __author__
 
 catub.version = __version__
 catub.tgbot.version = __version__
-LOGS = logging.getLogger("CatUserbot")
+LOGS = logging.getLogger("WarUserbot")
 bot = catub
 
 StartTime = time.time()
 catversion = "3.0.4"
 
 if Config.UPSTREAM_REPO == "badcat":
-    UPSTREAM_REPO_URL = "https://github.com/waruserbot/catuserbot"
+    UPSTREAM_REPO_URL = "https://github.com/xAbhish3k/waruserbot"
 elif Config.UPSTREAM_REPO == "goodcat":
-    UPSTREAM_REPO_URL = "https://github.com/waruserbot/catuserbott"
+    UPSTREAM_REPO_URL = "https://github.com/xAbhish3k/waruserbot"
 else:
     UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
 
