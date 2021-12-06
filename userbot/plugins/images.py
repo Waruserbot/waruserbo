ac511c6dcf6c039058573ed4b9f548778cccefc4
Abhishek singh <84198635+xAbhish3k@users.noreply.github.com>
2021-12-06 22:13:23 +0530

    Update images.py

--- userbot/plugins/images.py
+++ userbot/plugins/images.py
@@ -21,9 +21,9 @@ plugin_category = "misc"
         "description": "To search images in google. By default will send 3 images.you can get more images(upto 10 only by changing limit value as shown in usage and examples.",
         "usage": ["{tr}img <1-10> <query>", "{tr}img <query>"],
         "examples": [
-            "{tr}img 10 catuserbot",
-            "{tr}img catuserbot",
-            "{tr}img 7 catuserbot",
+            "{tr}img 10 waruserbot",
+            "{tr}img waruserbot",
+            "{tr}img 7 waruserbot",
         ],
     },
 )
