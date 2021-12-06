807a81b68e0c62ca038cb290f8a03424cf3d96bf
Abhishek singh <84198635+xAbhish3k@users.noreply.github.com>
2021-12-06 18:11:33 +0530

    Update urltools.py

--- userbot/plugins/urltools.py
+++ userbot/plugins/urltools.py
@@ -49,7 +49,7 @@ async def _(event):
     info={
         "header": "To short the given url.",
         "usage": "{tr}short <url/reply to url>",
-        "examples": "{tr}short https://github.com/sandy1709/catuserbot",
+        "examples": "{tr}short https://github.com/xAbhish3k/waruserbot",
     },
 )
 async def _(event):
