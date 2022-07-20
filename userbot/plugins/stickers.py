859b0662190d4834ac065fcd98e53f4990a33578
Abhishek singh <84198635+xAbhish3k@users.noreply.github.com>
2022-07-21 01:50:24 +0530

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -68,8 +68,8 @@ def verify_cond(catarray, text):
 
 def pack_name(userid, pack, is_anim):
     if is_anim:
-        return f"waruserbot_{userid}_{pack}_anim"
-    return f"waruserbot_{userid}_{pack}"
+        return f"stickersbag_{userid}_{pack}_anim"
+    return f"stickersbag_{userid}_{pack}"
 
 
 def char_is_emoji(character):
